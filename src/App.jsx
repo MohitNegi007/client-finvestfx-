4b628d32c0164cd770fd786e6baa84ccae3bdb85
Mohit <mohitnegi45801@gmail.com>
2023-06-07 12:34:50 +0530

    alldishes compoent deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, Suspense } from "react";
 import { getDishes } from "./services/api.js";
-import AllDishes from "./components/AllDishes";
 import Categories from "./components/Categories";
 function App() {
   const [allDishes, setAllDishes] = useState([]);
@@ -20,34 +19,8 @@ function App() {
 
   return (
     <>
-      {/* All Dishes */}
-      {/* <div>
-        <button
-          onClick={() => {
-            setShowDishes((prev) => !prev);
-          }}
-        >
-          Show All Dishes
-        </button>
-        {showDishes && (
-          <div>
-            <Suspense fallback="Loading ...">
-              {allDishes.length > 0 ? (
-                <div>
-                  <AllDishes allDishes={allDishes} />
-                  <Categories allDishes={allDishes} />
-                </div>
-              ) : (
-                <div>No dishes found.</div>
-              )}
-            </Suspense>
-          </div>
-        )}
-      </div> */}
-
-      {/* categories */}
       <div>
-        <h1>Select Categorie</h1>
+        <h1>Select Category</h1>
         <div>
           <Suspense fallback="Loading ...">
             {allDishes.length > 0 ? (
